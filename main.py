ecbb321e7b161bf901a8f6e9e43fcd178a3c15fd
Lolbartik89 <vladoleksuk0@gmail.com>
2026-01-23 19:02:07 +0100

    update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 while True:
+    # Get user input and strip chars it 
     user_action = input("Type add, show, edit, complete or exit:")
     user_action = user_action.strip()
 
-    match user_action:
+    match user_action:   
         case "add":
             todo = input("Enter a todo: ") + "\n" 
             file = open("files/todos.txt", "r")
@@ -10,6 +11,7 @@ while True:
             file.close()
 
             todos.append(todo)
+            
 
             file = open("files/todos.txt", "w")
             file.writelines(todos)
@@ -18,7 +20,11 @@ while True:
             file = open("files/todos.txt", "r")
             todos = file.readlines()
             file.close()
+
+            # new_todos = [item.strip('\n') for item in todos]
+
             for index, item in enumerate(todos):
+                item = item.strip('\n')
                 print (f"{index + 1}-{item.capitalize()}")
                 
                 
