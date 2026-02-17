1f321dcaa3bab034a1078e99d13ea07548736cbd
Lolbartik89 <vladoleksuk0@gmail.com>
2026-02-17 20:10:03 +0100

    feat: enhance todo management with edit functionality and update UI layout

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos_local(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My to-do App', 
-                   layout=[[label], [input_box, add_button]], 
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]], 
                    font=('Helvetica', 20))
 while True:
 
@@ -19,6 +22,20 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+              todo_to_edit = values['todos'][0]
+              new_todo = values['todo']
+
+              todos = functions.get_todos_local()
+              index = todos.index(todo_to_edit)
+              todos[index] = new_todo
+              functions.write_todos(todos)
+              window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+              
         case sg.WIN_CLOSED:
                 break
 
