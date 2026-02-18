bf284e6d1a4916d7c791579dbebfcf2c43328e10
Lolbartik89 <vladoleksuk0@gmail.com>
2026-02-18 19:21:31 +0100

    feat: add complete functionality to todo management and update UI layout

--- gui.py
+++ gui.py
@@ -7,8 +7,11 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos_local(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+layout = [[label], 
+          [input_box, add_button], 
+          [list_box, edit_button, complete_button]]
 
 window = sg.Window('My to-do App', 
                    layout= layout, 
@@ -34,6 +37,13 @@ while True:
               todos[index] = new_todo
               functions.write_todos(todos)
               window['todos'].update(values=todos)
+        case "Complete":
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos_local()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
