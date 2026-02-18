7ec06cff612af6203a9e787fe8f31f7d5ea1d723
Lolbartik89 <vladoleksuk0@gmail.com>
2026-02-18 21:57:44 +0100

    feat: add exit button functionality to the todo app GUI

--- gui.py
+++ gui.py
@@ -8,10 +8,12 @@ list_box = sg.Listbox(values=functions.get_todos_local(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit", button_color=('white', 'red'))
 
 layout = [[label], 
           [input_box, add_button], 
-          [list_box, edit_button, complete_button]]
+          [list_box, edit_button, complete_button], 
+          [exit_button]]
 
 window = sg.Window('My to-do App', 
                    layout= layout, 
@@ -44,6 +46,8 @@ while True:
                 functions.write_todos(todos)
                 window['todos'].update(values=todos)
                 window['todo'].update(value="")
+        case "Exit":
+                break
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
